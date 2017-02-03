f59d75bd32b9dd544baa096a19d202696712beb5
alex diaz <alexdiaz094@gmail.com>
2017-02-03 11:49:07 -0500

    npm install

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/auth', authRoutes);
 app.use('/user', userRoutes);
-app.use('/users', usersRoutes);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
