43c30de29e6f0a2263aa605e5ce2eb4aaa29dfa3
ant-mejia <ps@antmejia.com>
2017-02-05 00:38:52 -0500

    Updated messages table

---
+++ db/migrations/20170205053226-update-messages.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+    queryInterface.addColumn(
+      'Messages',
+      'Votes', {
+        type: Sequelize.INTEGER,
+        defaultValue: 0,
+        allowNull: false
+      }
+    )
+  },
+
+  down: function(queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
