e3ff566076c5a489e165452255b01d5ed1560142
alex diaz <alexdiaz094@gmail.com>
2017-02-06 13:23:22 -0500

    edit app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -27,24 +26,10 @@ require('dotenv').config();
 //use edditing
 app.use(methodOverride('_method'));
 
-=======
-const express = require('express');
-const path = require('path');
-const favicon = require('serve-favicon');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-// add new modules and files here
-const session = require('express-session');
-const passport = require('passport');
 
-const index = require('./routes/index');
-const authRoutes = require('./routes/auth.js');
-const userRoutes = require('./routes/user.js');
-const app = express();
-// load environment variables
-require('dotenv').config();
->>>>>>> b7f7312315e069c4a536ca48dc05196b3cf3476b
+// add new modules and files here
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -55,38 +40,26 @@ app.use(logger('dev'));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-<<<<<<< HEAD
+
 app.use(cookieParser());
 app.use(require('node-sass-middleware')({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
   indentedSyntax: false,
   sourceMap: true
-=======
+
 // add new express-session and passport middleware here
 // telling express to use express-session for secretKey
 app.use(session({
   secret: process.env.SECRET_KEY,
   resave: false,
   saveUnintialized: true
->>>>>>> b7f7312315e069c4a536ca48dc05196b3cf3476b
+
 }));
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(express.static(path.join(__dirname, 'public')));
-
-<<<<<<< HEAD
-// added for express-session and passport require
-app.use(session({
-  secret: process.env.SECRET_KEY,
-  resave: false,
-  saveUninitialized: true
-}));
-//not sure if i should add
-app.use(passport.initialize());
-app.use(passport.session());
-
 
 
 app.use('/', index);
@@ -95,13 +68,6 @@ app.use('/', index);
 app.use('/auth', authRoutes);
 app.use('/user', userRoutes);
 app.use('/comments', commentRoutes);
-=======
-// add route middleware here
-app.use('/', index);
-app.use('/auth', authRoutes);
-app.use('/user', userRoutes);
-
->>>>>>> b7f7312315e069c4a536ca48dc05196b3cf3476b
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -124,4 +90,4 @@ app.listen(port);
 console.log("Listening on port " + port);
 
 module.exports = app;
-//sequelize model:create --name Users --attributes "username:string(255) password:text email:string(128) firstName:string(255) lastName:string(255) rank:int"
+
