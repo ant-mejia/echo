e4cebb635b6072857cac010e63637940a08a5cf8
ant-mejia <ps@antmejia.com>
2017-03-01 20:41:12 -0500

    Updated Footer

--- db/models/messages.js
+++ db/models/messages.js
@@ -3,7 +3,8 @@ module.exports = function(sequelize, DataTypes) {
   var Messages = sequelize.define('Messages', {
     originId: DataTypes.STRING(255),
     content: DataTypes.TEXT,
-    location: DataTypes.TEXT
+    location: DataTypes.TEXT,
+    votes: DataTypes.INTEGER
   }, {
     classMethods: {
       associate: function(models) {
--- routes/message.js
+++ routes/message.js
@@ -10,12 +10,16 @@ const moment = require('moment');
 //route to display the show page with the message passed in
 router.get('/:id', authHelpers.loginRequired, function(req, res, next) {
   models.Messages.findById(req.params.id).then(function(message) {
-    res.render('message/show', {
-      title: 'message',
-      message: message,
-      user: req.user,
-      moment: moment
-    });
+    if (message) {
+      res.render('message/show', {
+        title: 'message',
+        message: message,
+        user: req.user,
+        moment: moment
+      });
+    } else {
+      res.redirect('/');
+    }
   });
 });
 
